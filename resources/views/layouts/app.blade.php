be9c4800c0b857d5c97d87d6ff375c753375a31b
sumitchoudharySEM <sumitsem2004@gmail.com>
2022-09-18 01:26:54 +0530

    pushing card

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     @include('layouts.metatags')
     @include('layouts.gtags')
     @include('layouts.styles')
-    <title>{{ $title ?? 'Semikolan Blogs | Never Stop Learning' }}</title>
+    <title>{{ $title ?? 'BlogRoom' }}</title>
     <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
